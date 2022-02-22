280b8ad6b23eb08f6447d2dc83af566dba00250b
Tolubaria <85737416+Tolubaria@users.noreply.github.com>
2022-02-22 13:37:05 +0200

    fix

---
+++ src/assets/js/modalNav.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById('sidebarToggle');
+const sidebar = document.getElementById('sidebar');
+
+burger.addEventListener('click', event => {
+	document.body.classList.toggle('shov-sidebar')
+
+});
